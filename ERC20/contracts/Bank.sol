fa2ebdb3136d5a514394bb384406deed70cb83f9
heku777 <chotips08@gmail.com>
2023-01-05 13:43:19 +0900

    Implementation of the ability to deposit and withdraw ERC20 tokens

---
+++ ERC20/contracts/Bank.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Bank {
+    address owner;
+    mapping(bytes32 => address) public whitelistedTokens;
+    mapping(address => mapping(bytes32 => uint256)) public accountBalances;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function whitelistToken(bytes32 symbol, address tokenAddress) external {
+        require(msg.sender == owner, 'This function is not public');
+
+        whitelistedTokens[symbol] = tokenAddress;
+    }
+
+    function getWhitelistedTokenAddresses(bytes32 token) external returns(address) {
+        return whitelistedTokens[token];
+    }
+
+    function depositTokens(uint256 amount, bytes32 symbol) external {
+        accountBalances[msg.sender][symbol] += amount;
+        ERC20(whitelistedTokens[symbol]).transferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdrawTokens(uint256 amount, bytes32 symbol) external {
+        require(accountBalances[msg.sender][symbol] >= amount, 'Insufficent funds');
+
+        accountBalances[msg.sender][symbol] -= amount;
+        ERC20(whitelistedTokens[symbol]).transfer(msg.sender, amount);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # smart-contract-test
 Smart Contract Test Repository
+
+## Upgradable Contract
+
+- [upgradable](./upogradable)
+
+## UniswapV3
+
+- [uniswapB3](./uniswapV3)
