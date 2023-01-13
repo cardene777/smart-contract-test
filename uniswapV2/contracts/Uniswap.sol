6d1924599cdb7eb880e1e842940a7c5748fd63bd
heku777 <chotips08@gmail.com>
2023-01-14 07:56:50 +0900

    💫 update Uniswap.sol

--- uniswapV2/contracts/Uniswap.sol
+++ uniswapV2/contracts/Uniswap.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./interfaces/Uniswap.sol";
+import "./interfaces/IUniswap.sol";
 
-contract TestUniswap {
+contract Uniswap {
     address private constant UNISWAP_V2_ROUTER =
         0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
@@ -21,22 +21,22 @@ contract TestUniswap {
 
         address[] memory path;
         if (_tokenIn == WETH || _tokenOut == WETH) {
-        path = new address[](2);
-        path[0] = _tokenIn;
-        path[1] = _tokenOut;
+            path = new address[](2);
+            path[0] = _tokenIn;
+            path[1] = _tokenOut;
         } else {
-        path = new address[](3);
-        path[0] = _tokenIn;
-        path[1] = WETH;
-        path[2] = _tokenOut;
+            path = new address[](3);
+            path[0] = _tokenIn;
+            path[1] = WETH;
+            path[2] = _tokenOut;
         }
 
         IUniswapV2Router(UNISWAP_V2_ROUTER).swapExactTokensForTokens(
-        _amountIn,
-        _amountOutMin,
-        path,
-        _to,
-        block.timestamp
+            _amountIn,
+            _amountOutMin,
+            path,
+            _to,
+            block.timestamp
         );
     }
 
@@ -47,14 +47,14 @@ contract TestUniswap {
     ) external view returns (uint) {
         address[] memory path;
         if (_tokenIn == WETH || _tokenOut == WETH) {
-        path = new address[](2);
-        path[0] = _tokenIn;
-        path[1] = _tokenOut;
+            path = new address[](2);
+            path[0] = _tokenIn;
+            path[1] = _tokenOut;
         } else {
-        path = new address[](3);
-        path[0] = _tokenIn;
-        path[1] = WETH;
-        path[2] = _tokenOut;
+            path = new address[](3);
+            path[0] = _tokenIn;
+            path[1] = WETH;
+            path[2] = _tokenOut;
         }
 
         // same length as path
